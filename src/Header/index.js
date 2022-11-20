90fa1f13f1b35174e9441a01bde96291f579910b
VFryzlova <vendulkaf@gmail.com>
2022-11-20 12:22:38 +0100

    today's date and name day in Header

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,17 +1,27 @@
 export const Header = () => {
     const element = document.createElement('header')
-    element.innerHTML = `
-        <nav>
-            <a href="/register">Register</a>
-            <a href="/login">Login</a>
-        </nav>
 
-        <div class="today">
-            <h1 class="hi">Hi Vendy!</h1>
-            <p class="date">It is 12.12.2022</p>
-            <p>has a name-day</p>
-            <input type="search" name="" id="" placeholder="Search.." />
-        </div>
-    `
+    // Today's date
+    const date = new Date(),
+    dateFormatted = date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear()
+
+    // Today's name day
+    fetch('https://svatky.adresa.info/json')
+        .then((response) => response.json())
+        .then((data) => element.innerHTML = `
+                <nav>
+                    <a href="/register">Register</a>
+                    <a href="/login">Login</a>
+                </nav>
+
+                <div class="today">
+                    <h1 class="hi">Hi Vendy!</h1>
+                    <p class="date">It is ${dateFormatted}</p>
+                    <p class="nameDay">${data[0].name} has a name-day</p>
+                    <input type="search" name="" id="" placeholder="Search.." />
+                </div>
+            `
+        )
+
     return element
 }
